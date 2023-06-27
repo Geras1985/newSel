c5d08bc3545e88a5a7bea7c5e2fdda5fc76e7f6d
gerasimgh <gerasgharibyan@mail.ru>
2023-06-27 13:18:37 +0400

    ss

--- src/test/java/com/example/allsel/Test0.java
+++ src/test/java/com/example/allsel/Test0.java
@@ -1,25 +1,25 @@
 package com.example.allsel;
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
-public class Test0 {
-    @Test
-    public static void test0() {
-        Assert.assertTrue(true);
-        System.out.println("TRUE");
-    }
-
-    @Test
-    public static void test1() {
-        Assert.assertTrue(false);
-        System.out.println("FALSE");
-    }
-
-    @Test
-    public void test2(){
-        Assert.assertEquals(2,2);
-        System.out.println("yes");
-    }
-
-}
+//import org.testng.Assert;
+//import org.testng.annotations.Test;
+//
+//public class Test0 {
+//    @Test
+//    public static void test0() {
+//        Assert.assertTrue(true);
+//        System.out.println("TRUE");
+//    }
+//
+//    @Test
+//    public static void test1() {
+//        Assert.assertTrue(false);
+//        System.out.println("FALSE");
+//    }
+//
+//    @Test
+//    public void test2(){
+//        Assert.assertEquals(2,2);
+//        System.out.println("yes");
+//    }
+//
+//}
